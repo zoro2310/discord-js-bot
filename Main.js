ddd3bd6767fbb7d022b8166677305efc0886ae74
Jitendra41293 <62878884+Jitendra41293@users.noreply.github.com>
2021-11-19 23:22:31 +0530

    created main for the program

---
+++ Main.js
@@ -0,0 +1,50 @@
+const { Client, Collection, Intents } = require('discord.js');
+const fs=require("fs");
+
+//Declaring Intents
+const client= new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_INTEGRATIONS,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGES
+      ]
+})
+
+
+//Calling Token
+const { token } = require("./config.json");
+
+
+// Get all commands and put it in client.commands
+const commands = [];
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+    client.commands.set(command.data.name, command);
+}
+
+
+//EVENT HANDLER
+const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith('.js'));
+for( const file of eventFiles){
+    const event = require(`./events/${file}`);
+
+    if(event.once){
+        client.once(event.name,(...args)=>event.execute(...args,commands));
+    }else{
+        client.on(event.name,(...args)=>event.execute(...args,commands));
+    }
+}
+
+
+// Token
+client.login(token);
