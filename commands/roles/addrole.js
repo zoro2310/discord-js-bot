967e4e7b7d205e26f8c5bf0e5fea00a2d60f50b7
Jitendra41293 <62878884+Jitendra41293@users.noreply.github.com>
2021-11-20 16:18:36 +0530

    added permissions setting

--- commands/roles/addrole.js
+++ commands/roles/addrole.js
@@ -4,9 +4,23 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('addrole')
 		.setDescription('Add Role to the mentioned user')
-        .addUserOption(option => option.setName('target').setDescription('Select a user'))
-        .addRoleOption(option => option.setName('role').setDescription('Select a role')),
+        .addUserOption(option => 
+			option
+				.setName('target')
+				.setDescription('Select a user')
+				.setRequired(true)
+		)
+        .addRoleOption(option => 
+			option
+				.setName('role')
+				.setDescription('Select a role')
+				.setRequired(true)
+		),
 	async execute(interaction) {
+		if (!interaction.member.permissions.has("MANAGE_ROLES")){
+			await interaction.reply(`No Perms`);
+			return;
+		}
         const member = interaction.options.getMember('target');
         const role = interaction.options.getRole('role');
 		await interaction.reply(`Added Role ${role} To ${member}`);
--- commands/roles/removerole.js
+++ commands/roles/removerole.js
@@ -4,12 +4,26 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('removerole')
 		.setDescription('Role Role of the mentioned user')
-        .addUserOption(option => option.setName('target').setDescription('Select a user'))
-        .addRoleOption(option => option.setName('role').setDescription('Select a role')),
+        .addUserOption(option => 
+			option
+				.setName('target')
+				.setDescription('Select a user')
+				.setRequired(true)
+		)
+        .addRoleOption(option => 
+			option
+				.setName('role')
+				.setDescription('Select a role')
+				.setRequired(true)
+		),
 	async execute(interaction) {
+		if (!interaction.member.permissions.has("MANAGE_ROLES")){
+			await interaction.reply(`No Perms`);
+			return;
+		}
         const member = interaction.options.getMember('target');
         const role = interaction.options.getRole('role');
-		await interaction.reply(`Added Role ${role} To ${member}`);
+		await interaction.reply(`Removed Role ${role} To ${member}`);
         await member.roles.remove(role);
 	},
 };
