ea06e47c35abeb56d4dd48e041a2d000a9c282d9
Jitendra41293 <62878884+Jitendra41293@users.noreply.github.com>
2021-11-25 16:15:14 +0530

    completed profile command

--- server/getprofile.js
+++ server/getprofile.js
@@ -1,12 +1,11 @@
 const { MongoClient } = require("mongodb");
 var url = "mongodb://localhost:27017/";
-const { MessageEmbed } = require('discord.js');
-
 
 module.exports = {
     name: "getprofile",
-    async execute(interaction) {
+    async execute(interaction, user) {
 
+        console.log("fetched");
         const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
         var dbo = client.db("discord");
@@ -15,24 +14,15 @@ module.exports = {
         var result = await dbo.collection("guilds").find().toArray();
         console.log("data fetched");
         for (const o_g of result) {
-            if (interaction.guildid == o_g.guild_id) {
+            if (interaction.guild.id == o_g.guild_id) {
+                console.log("guild fetched");
                 for (const o_u of o_g.users) {
-                    if (interaction.user.id == o_u.user_id) {
+                    if (user.id == o_u.user_id) {
+                        console.log("user fetched");
                         //fetch the user data
                         var user_data = o_u;
                         //reply interaction with user data
-                        const exampleEmbed = new MessageEmbed()
-                            .setColor('#0099ff')
-                            .setTitle('Your Server Profile')
-                            .setAuthor(interaction.user.username, interaction.user.avatarURL())
-                            .addFields(
-                                { name: 'XP', value: user_data.xp, inline: true },
-                                { name: 'LEVEL', value: user_data.level, inline: true },
-                                { name: 'WARNINGS', value: user_data.warnings, inline: true },
-                            )
-
-                        interaction.reply({embed: exampleEmbed});
-                        break;
+                        return user_data;
                     }
                 }
                 break;
