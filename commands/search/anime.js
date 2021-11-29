41a18580d9b90c262c65dd3a0068145cc822524b
Jitendra41293 <62878884+Jitendra41293@users.noreply.github.com>
2021-11-29 11:42:29 +0530

    file updated

---
+++ commands/search/anime.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fun = require(`../../search/getanime.js`);
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('anime')
+		.setDescription('Search for an anime')
+        .addStringOption(option =>
+			option
+				.setName('name')
+				.setDescription('Enter the name of user')
+		),
+	async execute(interaction) {
+        const name = interaction.options.getString('name');
+        fun.execute(interaction,name);
+	},
+};
---
+++ commands/search/manga.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fun = require(`../../search/getmanga.js`);
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('manga')
+		.setDescription('Search for a manga')
+        .addStringOption(option =>
+			option
+				.setName('name')
+				.setDescription('Enter the name of user')
+		),
+	async execute(interaction) {
+        const name = interaction.options.getString('name');
+        fun.execute(interaction,name);
+	},
+};
